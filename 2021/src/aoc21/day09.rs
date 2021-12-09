7b46e100a5c822c7ca6c82196f4396997f80e2f0
Andreas K. Berg <andreaskb98@gmail.com>
2021-12-09 22:15:36 +0100

    day 9 done, rs

--- 2021/src/aoc21/day09.rs
+++ 2021/src/aoc21/day09.rs
@@ -1,50 +1,142 @@
 static DAY: i32 = 9;
 
+use std::collections::VecDeque;
 
-pub fn input() -> Vec<usize> {
-    vec![0]
+fn parse_line(line: &str) -> Vec<i32> {
+    line.chars().map(|c| c as i32 - '0' as i32).collect::<_>()
 }
 
-pub fn part1(inputs: &Vec<usize>) -> usize {
-    0
+pub fn input() -> Vec<Vec<i32>> {
+    use std::io::{prelude::*, BufReader};
+    let f = crate::aoc::input_file(DAY).unwrap();
+    let f = BufReader::new(f);
+    f.lines().map(|line|parse_line(&line.unwrap())).collect::<_>()
 }
 
-pub fn part2(inputs: &Vec<usize>) -> usize {
-    0
+/**Find all local minimas, points that are lower than all of their
+ * neighbours */
+pub fn part1(inputs: &Vec<Vec<i32>>) -> i32 {
+    let mut sum = 0;
+
+    for i in 0..inputs.len() {
+        for j in 0..inputs[0].len() {
+            if [(0,-1),(0,1),(1,0),(-1,0)]
+            .iter()
+            .filter(|(di,dj)| (i>0 || *di>-1) && (j>0 || *dj>-1))
+            .all(|(di,dj)|
+                inputs[i][j] < *inputs.get((i as isize + di) as usize)
+                    .unwrap_or(&vec![])
+                    .get((j as isize+dj) as usize)
+                    .unwrap_or(&9)
+            ) {
+                sum += inputs[i][j] + 1;
+            }
+        }
+    }
+    sum
 }
 
+/**Return vector of local minimas */
+fn local_min(inputs: &Vec<Vec<i32>>) -> Vec<(usize, usize)> {
+    (0..inputs.len())
+    .flat_map(|i| (0..inputs[0].len())
+        .filter(move|j|
+            [(0,-1),(0,1),(1,0),(-1,0)]
+            .iter()
+            .filter(|(di,dj)| (i>0 || *di>-1) && (*j>0 || *dj>-1))
+            .all(|(di,dj)|
+                inputs[i][*j] < *inputs.get((i as isize + di) as usize)
+                    .unwrap_or(&vec![])
+                    .get((*j as isize+dj) as usize)
+                    .unwrap_or(&9)
+            )
+        )
+        .map(move|j| (i,j))
+    ).collect::<Vec<(usize, usize)>>()
+}
+
+/**Fill all basins and return the product of the 3 largest ones */
+pub fn part2(inputs: &Vec<Vec<i32>>) -> u32 {
+    #[allow(non_snake_case)]
+    let M = inputs.len();
+    #[allow(non_snake_case)]
+    let N = inputs[0].len();
+    // Track visited points
+    let mut visited = vec![vec![0; inputs[0].len()]; inputs.len()];
+    let low_points = local_min(inputs);
+    let mut queue = low_points.iter().map(|&p| VecDeque::from(vec![p])).collect::<Vec<_>>();
+    // Track size of each basin
+    let mut b_size = vec![0;low_points.len()];
+
+    // Floodfill
+    for basin in 0..queue.len() {
+        eprintln!("\n== {} ==\n", basin);
+        while let Some((i,j)) = queue[basin].pop_front() {
+            visited[i][j] = basin+1;
+            // Possible neightbours
+            let nbs = [(0,-1),(0,1),(1,0),(-1,0)]
+                .iter()
+                // Edge filter
+                .filter(|&(di,dj)| (i>0 || *di>-1) && (i<M-1 || *di<1) && (j>0 || *dj>-1) && (j<N-1 || *dj<1))
+                .map(|(di,dj)| ((i as isize + di) as usize, (j as isize + dj) as usize))
+                .filter(|&(ii,jj)| basin+1 != visited[ii][jj])
+                .collect::<Vec<_>>();
+            // Just a floodfill with 9's as delimiters...
+            if inputs[i][j] < 9 {
+                b_size[basin] += 1;
+                for nb in nbs {
+                    visited[nb.0][nb.1] = basin+1;
+                    queue[basin].push_back(nb);
+                }
+            }
+        }
+
+        // Debug
+        let colors = ["0;31m", "0;32m", "0;33m", "0;34m", "0;35m", "0;36m", "0;37m", "1;30m", "1;31m", "1;32m", "1;33m", "1;34m", "1;35m", "1;36m"];
+        for (i, line) in (&visited).iter().enumerate() {
+            for (j, p) in line.iter().enumerate() {
+                if inputs[i][j] == 9 {
+                    eprint!("\x1b[0;41m{}\x1b[0m", inputs[i][j]);
+                } else {
+                    eprint!("\x1b[{}{}\x1b[0m", if *p==0 { "0m" } else { colors[*p % 14] }, inputs[i][j]);
+                }
+            }
+            println!("");
+        }
+        eprintln!("{}: {}", basin, b_size[basin]);
+    }
+
+    // Return product of three largest basins size
+    b_size.sort_by(|a,b| b.cmp(a));
+    b_size[0] * b_size[1] * b_size[2]
+}
+// 349440 too low
 
 /* TESTS */
 #[allow(unused)]
-static TEST_LINE: &'static str = "\
-acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | \
-cdfeb fcadb cdfeb cdbaf\
-";
-#[allow(unused)]
 static TEST_INPUT: &'static [&str] = &[
-    "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
-    "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
-    "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
-    "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
-    "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
-    "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
-    "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
-    "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
-    "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
-    "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
+    "2199943210",
+    "3987894921",
+    "9856789892",
+    "8767896789",
+    "9899965678",
 ];
-
 
 #[test]
 fn test_day09_part1() {
-    // let inputs = TEST_INPUT;
-    // assert_eq!(part1(&inputs), 26);
+    let inputs = TEST_INPUT.iter().map(|line|parse_line(*line)).collect::<Vec<_>>();
+    assert_eq!(part1(&inputs), 15);
 }
 
 #[test]
 fn test_day09_part2() {
-    // let inputs = TEST_INPUT;
-    // assert_eq!(part2(&inputs), 61229);
+    let inputs = TEST_INPUT.iter().map(|line|parse_line(*line)).collect::<Vec<_>>();
+    assert_eq!(part2(&inputs), 1134);
+}
+
+#[test]
+fn test_day09_parse_line() {
+    assert_eq!(parse_line(TEST_INPUT[0]), vec![2,1,9,9,9,4,3,2,1,0]);
 }
 
 #[allow(unused)]
