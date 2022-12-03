325be15a88910f37fef6d7f530bc0fa42e3b819d
Andreas K. Berg <andreaskb98@gmail.com>
2022-12-03 06:49:56 +0100

    2022 day 3, rs

--- 2022/src/aoc22/day03.rs
+++ 2022/src/aoc22/day03.rs
@@ -1,30 +1,72 @@
 static DAY: usize = 03;
 
-pub fn input() -> Vec<u32> {
-    crate::aoc::input_raw(1)
-        .lines()
-        .map(|ls| ls.parse::<_>().unwrap())
-        .collect()
+pub fn input() -> String {
+    crate::aoc::input_raw(DAY as i32)
+        // .lines()
+        // .map(|line| line.split_at(line.len()/2))
+        // //.map(|ls| ls.parse::<_>().unwrap())
+        // .collect()
 }
 
-pub fn part1(inputs: &Vec<u32>) -> u32 {
-    todo!();
-    0
+fn parse_line(line: &str) -> Vec<u32> {
+    line.chars()
+    .map(|c| {
+        let c = c as u8;
+        if c <= 'Z' as u8 {
+            (c - 'A' as u8 + 27) as u32
+        } else {
+            (c - 'a' as u8 + 1) as u32
+        }
+    })
+    .collect::<_>()
 }
 
-pub fn part2(inputs: &Vec<u32>) -> u32 {
-    todo!();
-    0
+pub fn part1(inputs: &str) -> u64 {
+    inputs.lines()
+    .map(|line| line.split_at(line.len() / 2))
+    .map(|(s1, s2)| (parse_line(s1), parse_line(s2)))
+    .map(|(s1, s2)| {
+        s1.iter()
+        .filter(|c| s2.contains(c))
+        .map(|c|*c as u64).next().unwrap()
+    })
+    .sum::<_>()
+}
+
+pub fn part2(inputs: &str) -> u64 {
+    let inputs = inputs.lines()
+    .map(|line| parse_line(line))
+    .collect::<Vec<_>>();
+
+    inputs.chunks(3)
+    .map(|tri| {
+        tri[0].iter()
+        .filter(|c| tri[1].contains(c) && tri[2].contains(c))
+        .map(|c|*c as u64).next().unwrap()
+    })
+    .sum::<_>()
 }
 
 #[test]
-fn test_day01_part1() {
-    // TODO
+fn test_day03_part1() {
+    let inputs = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+    assert_eq!(part1(&inputs), 157);
 }
 
 #[test]
-fn test_day01_part2() {
-    // TODO
+fn test_day03_part2() {
+    let inputs = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+    assert_eq!(part2(&inputs), 70);
 }
 
 #[allow(unused)]
