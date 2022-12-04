ddf20d0470672249bbab37208531e2e5decae776
Andreas K. Berg <andreaskb98@gmail.com>
2022-12-04 06:55:47 +0100

    update main.rs

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,23 +1,27 @@
 mod aoc22;
 pub mod aoc;
 
-static FIRST_YEAR: usize = 2022;
+static YEAR: usize = 2022;
 static RUNS: [[fn(); 25]; 1] = [
     aoc22::RUNS
 ];
 
 use std::env;
+use chrono::{self, Datelike};
+#[allow(unused)]
 fn help() {
     println!("usage:
-aoc <year> <day>");
+aoc2022 <day>");
 }
 
 fn main() {
     let mut args = env::args();
     let _application = args.next().unwrap();
-    let year = args.next().unwrap_or_default().parse::<usize>().unwrap_or(FIRST_YEAR+RUNS.len()-1);
-    let day  = args.next().unwrap_or_default().parse::<usize>().unwrap_or(RUNS[year-FIRST_YEAR].len());
-    println!("{}, day {}", year, day);
+    //let year = args.next().unwrap_or_default().parse::<usize>().unwrap_or(FIRST_YEAR+RUNS.len()-1);
+    let day  = args.next().unwrap_or_default()
+        .parse::<usize>()
+        .unwrap_or(chrono::Utc::now().date_naive().day() as usize);
+    println!("{}, day {}", YEAR, day);
     //aoc22::day02::run();
-    RUNS[year - FIRST_YEAR][day - 1]();
+    RUNS[0][day - 1]();
 }
