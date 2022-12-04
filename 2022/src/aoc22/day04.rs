23d1b1dacd81e8869bdda8bf938168e8b22a0ed3
Andreas K. Berg <andreaskb98@gmail.com>
2022-12-04 06:55:14 +0100

    2022 day 4, rs

--- 2022/src/aoc22/day04.rs
+++ 2022/src/aoc22/day04.rs
@@ -1,30 +1,51 @@
 static DAY: usize = 04;
 
-pub fn input() -> Vec<u32> {
-    crate::aoc::input_raw(1)
-        .lines()
-        .map(|ls| ls.parse::<_>().unwrap())
-        .collect()
+pub fn input() -> String {
+    crate::aoc::input_raw(4)
+        // .lines()
+        // .map(|ls| ls.parse::<_>().unwrap())
+        // .collect()
 }
 
-pub fn part1(inputs: &Vec<u32>) -> u32 {
-    todo!();
-    0
+pub fn part1(inputs: &str) -> u32 {
+    inputs.lines()
+    .map(|line| sscanf::sscanf!(line, "{}-{},{}-{}", usize,usize,usize,usize).unwrap())
+    .filter(|(a0,a1,b0,b1)| (a0<=b0&&b1<=a1) || (b0<=a0&&a1<=b1))
+    .count() as u32
 }
 
-pub fn part2(inputs: &Vec<u32>) -> u32 {
-    todo!();
-    0
+pub fn part2(inputs: &str) -> u32 {
+    inputs.lines()
+    .map(|line| sscanf::sscanf!(line, "{}-{},{}-{}", usize,usize,usize,usize).unwrap())
+    .filter(|(a0,a1,b0,b1)|
+        (a0..=a1).contains(&b0) ||
+        (a0..=a1).contains(&b1) ||
+        (b0..=b1).contains(&a0) ||
+        (b0..=b1).contains(&a1)
+    )
+    .count() as u32
 }
 
 #[test]
-fn test_day01_part1() {
-    // TODO
+fn test_day4_part1() {
+    let inputs = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+    assert_eq!(part1(inputs), 2);
 }
 
 #[test]
-fn test_day01_part2() {
-    // TODO
+fn test_day4_part2() {
+    let inputs = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+    assert_eq!(part2(inputs), 4);
 }
 
 #[allow(unused)]
