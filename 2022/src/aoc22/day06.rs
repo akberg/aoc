ed3c1211a5d8f3bf7b72a48776465c062b6034d2
Andreas K. Berg <andreaskb98@gmail.com>
2022-12-06 06:55:17 +0100

    2022 day 6, rs

--- 2022/src/aoc22/day06.rs
+++ 2022/src/aoc22/day06.rs
@@ -1,30 +1,56 @@
 static DAY: usize = 06;
 
+
 pub fn input() -> String {
     crate::aoc::input_raw(6)
-        //.lines()
-        //.map(|ls| ls.parse::<_>().unwrap())
-        //.collect()
+}
+
+/// Find the first window of size `len` in which all characters are distinct,
+/// and return the following index
+fn find_marker(inputs: &Vec<char>, len: usize) -> u32 {
+    (len-1..inputs.len())
+    .filter(|&i| {
+        (i-(len-2)..=i).all(|j| !inputs[i-(len-1)..j].contains(&inputs[j]))
+    })
+    .next().unwrap() as u32 + 1
 }
 
 pub fn part1(inputs: &str) -> u32 {
-    todo!();
-    0
+    let inputs = inputs.chars().collect::<Vec<_>>();
+    find_marker(&inputs, 4)
 }
 
 pub fn part2(inputs: &str) -> u32 {
-    todo!();
-    0
+    let inputs = inputs.chars().collect::<Vec<_>>();
+    find_marker(&inputs, 14)
 }
+// 2135 not correct
 
 #[test]
 fn test_day6_part1() {
-    // TODO
+    let inputs = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+    assert_eq!(part1(inputs), 5);
+    let inputs = "nppdvjthqldpwncqszvftbrmjlhg";
+    assert_eq!(part1(inputs), 6);
+    let inputs = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+    assert_eq!(part1(inputs), 10);
+    let inputs = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+    assert_eq!(part1(inputs), 11);
+
 }
 
 #[test]
 fn test_day6_part2() {
-    // TODO
+    let inputs = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+    assert_eq!(part2(inputs), 19);
+    let inputs = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+    assert_eq!(part2(inputs), 23);
+    let inputs = "nppdvjthqldpwncqszvftbrmjlhg";
+    assert_eq!(part2(inputs), 23);
+    let inputs = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+    assert_eq!(part2(inputs), 29);
+    let inputs = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+    assert_eq!(part2(inputs), 26);
 }
 
 #[allow(unused)]
