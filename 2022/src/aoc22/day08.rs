5aa432f9d49cb281fa1229e6bb6918b83e078425
Andreas K. Berg <andreaskb98@gmail.com>
2022-12-08 09:33:36 +0100

    2022 day 8, rs

--- 2022/src/aoc22/day08.rs
+++ 2022/src/aoc22/day08.rs
@@ -1,30 +1,66 @@
 static DAY: usize = 08;
 
-pub fn input() -> String {
+pub fn input() -> Vec<Vec<usize>> {
     crate::aoc::input_raw(8)
-        //.lines()
-        //.map(|ls| ls.parse::<_>().unwrap())
-        //.collect()
+        .lines()
+        .map(|ls| ls.chars().map(|c| c as usize - '0' as usize).collect::<Vec<_>>())
+        .collect()
 }
 
-pub fn part1(inputs: &str) -> u32 {
-    todo!();
-    0
+/// Number of trees that are visible from the edges (no diagonal)
+pub fn part1(inputs: &Vec<Vec<usize>>) -> usize {
+    inputs.iter().enumerate()
+    .map(|(y, row)| {
+        row.iter().enumerate()
+        .filter(|&(x,&e)| {
+            // For each direction, check if all trees are lower in at least one
+            // of them.
+            (0..x).all(|x0| inputs[y][x0] < e) ||
+            (0..y).all(|y0| inputs[y0][x] < e) ||
+            (x == row.len()-1 || (x+1..row.len()).all(|x0| inputs[y][x0] < e)) ||
+            (y == inputs.len()-1 || (y+1..row.len()).all(|y0| inputs[y0][x] < e))
+        }).count()
+    }).sum::<usize>()
 }
 
-pub fn part2(inputs: &str) -> u32 {
-    todo!();
-    0
+/// Number of trees that are visible from each tree
+pub fn part2(inputs: &Vec<Vec<usize>>) -> usize {
+    inputs.iter().enumerate()
+    .map(|(y, row)| {
+        row.iter().enumerate()
+        .map(|(x,&e)| {
+            // For each direction, count to the first taller or equally tall
+            // tree, then multiply the results.
+            (x - (0..x).rev().find(|&x0| inputs[y][x0] >= e).unwrap_or(0)) *
+            (y - (0..y).rev().find(|&y0| inputs[y0][x] >= e).unwrap_or(0)) *
+            ((x+1..row.len()).find(|&x0| inputs[y][x0] >= e).unwrap_or(row.len()-1) - x) *
+            ((y+1..inputs.len()).find(|&y0| inputs[y0][x] >= e).unwrap_or(inputs.len()-1) - y)
+        }).max().unwrap()
+    }).max().unwrap()
 }
 
 #[test]
 fn test_day8_part1() {
-    // TODO
+    let inputs = vec![
+        vec![3,0,3,7,3],
+        vec![2,5,5,1,2],
+        vec![6,5,3,3,2],
+        vec![3,3,5,4,9],
+        vec![3,5,3,9,0],
+    ];
+    assert_eq!(part1(&inputs), 21);
 }
 
 #[test]
 fn test_day8_part2() {
-    // TODO
+    let inputs = vec![
+        vec![3,0,3,7,3],
+        vec![2,5,5,1,2],
+        vec![6,5,3,3,2],
+        vec![3,3,5,4,9],
+        vec![3,5,3,9,0],
+    ];
+    assert_eq!(part2(&inputs), 8);
 }
 
 #[allow(unused)]
