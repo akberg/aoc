3d9f85e7310a303b7cb779f6e5a5da943187ba07
Andreas K. Berg <andreaskb98@gmail.com>
2022-12-18 10:25:26 +0100

    2022 day 18, rs

--- 2022/src/aoc22/day18.rs
+++ 2022/src/aoc22/day18.rs
@@ -1,30 +1,107 @@
 static DAY: usize = 18;
 
-pub fn input() -> String {
+type Surface = (usize, usize, usize);
+
+pub fn input() -> Vec<Surface> {
     crate::aoc::input_raw(18)
-        //.lines()
-        //.map(|ls| ls.parse::<_>().unwrap())
-        //.collect()
+        .lines()
+        .map(|line| sscanf::scanf!(line, "{},{},{}", usize, usize, usize).unwrap())
+        .collect()
 }
 
-pub fn part1(inputs: &str) -> u32 {
-    todo!();
-    0
+/// Decrease surface area of single cubes for each adjacent cube
+pub fn part1(inputs: &Vec<Surface>) -> u32 {
+    let mut sides = vec![6; inputs.len()];
+    for (i, cube) in inputs.into_iter().enumerate() {
+        for (j, other) in inputs.into_iter().enumerate().skip(i) {
+            if (cube.0 as isize - other.0 as isize).abs()
+                + (cube.1 as isize - other.1 as isize).abs()
+                + (cube.2 as isize - other.2 as isize).abs() == 1 {
+                sides[i] -= 1;
+                sides[j] -= 1;
+            }
+        }
+    }
+    sides.iter().sum()
 }
 
-pub fn part2(inputs: &str) -> u32 {
-    todo!();
-    0
+fn flood_fill(pt: Surface, map: &Vec<Vec<Vec<usize>>>, vis: &mut Vec<Vec<Vec<bool>>>) -> usize {
+    if vis[pt.0][pt.1][pt.2] {
+        return 0;
+    }
+    vis[pt.0][pt.1][pt.2] = true;
+    map[pt.0][pt.1][pt.2]
+    + if pt.0 == map.len()-1 { 0 } else {       flood_fill((pt.0+1,pt.1,pt.2), map, vis) }
+    + if pt.0 == 0 { 0 } else {                 flood_fill((pt.0-1,pt.1,pt.2), map, vis) }
+    + if pt.1 == map[0].len()-1 { 0 } else {    flood_fill((pt.0,pt.1+1,pt.2), map, vis) }
+    + if pt.1 == 0 { 0 } else {                 flood_fill((pt.0,pt.1-1,pt.2), map, vis) }
+    + if pt.2 == map[0][0].len()-1 { 0 } else { flood_fill((pt.0,pt.1,pt.2+1), map, vis) }
+    + if pt.2 == 0 { 0 } else {                 flood_fill((pt.0,pt.1,pt.2-1), map, vis) }
+}
+
+
+/// Count adjacent cubes for every cell, then flood fill from corner to sum
+/// outer surface area.
+pub fn part2(inputs: &Vec<Surface>) -> usize {
+    let (x,y,z) = inputs.iter().fold((0,0,0), |(x,y,z),(i,j,k)| (x.max(*i),y.max(*j),z.max(*k)));
+    let mut map = vec![vec![vec![0usize;z+3];y+3];x+3];
+    let mut vis = vec![vec![vec![false;z+3];y+3];x+3];
+    for cube in inputs {
+        map[1+cube.0+1][1+cube.1][1+cube.2] += 1;
+        map[1+cube.0-1][1+cube.1][1+cube.2] += 1;
+        map[1+cube.0][1+cube.1+1][1+cube.2] += 1;
+        map[1+cube.0][1+cube.1-1][1+cube.2] += 1;
+        map[1+cube.0][1+cube.1][1+cube.2+1] += 1;
+        map[1+cube.0][1+cube.1][1+cube.2-1] += 1;
+        vis[1+cube.0][1+cube.1][1+cube.2] = true;
+    }
+
+    flood_fill((x+1,y+1,z+1), &map, &mut vis)
 }
 
 #[test]
 fn test_day18_part1() {
-    // TODO
+    let inputs = vec![
+        (1,1,1),
+        (2,1,1)
+    ];
+    assert_eq!(part1(&inputs), 10);
+    let inputs = vec![
+        (2,2,2),
+        (1,2,2),
+        (3,2,2),
+        (2,1,2),
+        (2,3,2),
+        (2,2,1),
+        (2,2,3),
+        (2,2,4),
+        (2,2,6),
+        (1,2,5),
+        (3,2,5),
+        (2,1,5),
+        (2,3,5),
+        ];
+    assert_eq!(part1(&inputs), 64);
 }
 
 #[test]
 fn test_day18_part2() {
-    // TODO
+    let inputs = vec![
+        (2,2,2),
+        (1,2,2),
+        (3,2,2),
+        (2,1,2),
+        (2,3,2),
+        (2,2,1),
+        (2,2,3),
+        (2,2,4),
+        (2,2,6),
+        (1,2,5),
+        (3,2,5),
+        (2,1,5),
+        (2,3,5),
+        ];
+    assert_eq!(part2(&inputs), 58);
 }
 
 #[allow(unused)]
