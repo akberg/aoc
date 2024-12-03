2a0b1ef693ff863a5b38acb18cce9276adb0fad6
Andreas Klavenes Berg <andreasklavenes.berg@arm.com>
2024-12-03 09:55:40 +0100

    Day 3 2024 solved

--- aoc_rs/src/aoc24/day03.rs
+++ aoc_rs/src/aoc24/day03.rs
@@ -1,31 +1,59 @@
+use regex::Regex;
+
 use super::YEAR;
 static DAY: usize = 03;
 
 pub fn input() -> String {
     crate::aoc::input_raw(YEAR, DAY)
-        //.lines()
-        //.map(|ls| ls.parse::<_>().unwrap())
-        //.collect()
+    //.lines()
+    //.map(|ls| ls.parse::<_>().unwrap())
+    //.collect()
 }
 
-pub fn part1(inputs: &str) -> u32 {
-    todo!();
-    0
+pub fn part1(inputs: &str) -> i64 {
+    // Match mul(a, b) where a and b a 1-3 digit integers.
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    re.captures_iter(inputs)
+        .map(|cap| {
+            cap.get(1).unwrap().as_str().parse::<i64>().unwrap()
+                * cap.get(2).unwrap().as_str().parse::<i64>().unwrap()
+        })
+        .sum::<_>()
 }
 
-pub fn part2(inputs: &str) -> u32 {
-    todo!();
-    0
+pub fn part2(inputs: &str) -> i64 {
+    let re = Regex::new(
+        r"(?<mul>mul\((?<a>\d{1,3}),(?<b>\d{1,3})\))|(?<open>do\(\))|(?<close>don't\(\))",
+    )
+    .unwrap();
+    let mut en = true;
+    let mut res = 0;
+    for cap in re.captures_iter(inputs) {
+        if let Some(_) = cap.name("mul") {
+            if en {
+                res += &cap["a"].parse::<i64>().unwrap() * &cap["b"].parse::<i64>().unwrap();
+            }
+        }
+        if let Some(_) = cap.name("open") {
+            en = true;
+        }
+        if let Some(_) = cap.name("close") {
+            en = false;
+        }
+    }
+    return res;
 }
 
 #[test]
 fn test_2024_day3_part1() {
-    // TODO
+    let test_inputs = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    assert_eq!(part1(test_inputs), 161);
 }
 
 #[test]
 fn test_2024_day3_part2() {
-    // TODO
+    let test_inputs = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+    assert_eq!(part2(test_inputs), 48);
 }
 
 #[allow(unused)]
@@ -47,5 +75,3 @@ pub fn run() {
     println!("Took {:?}", pt_start.elapsed().unwrap());
     println!("Total time: {:?}", start.elapsed().unwrap());
 }
-
-
