adb7fd8cb1ccbe8bde9e70015d4ad93f7ad13858
Andreas Klavenes Berg <andreasklavenes.berg@arm.com>
2024-12-09 08:56:56 +0100

    2024 day 8 solved

--- aoc_rs/src/aoc24/day08.rs
+++ aoc_rs/src/aoc24/day08.rs
@@ -1,29 +1,122 @@
+use std::collections::{HashMap, HashSet};
+
+use itertools::Itertools;
+
+use crate::aoc::print_img;
+
 use super::YEAR;
 static DAY: usize = 08;
 
 fn input() -> String {
     crate::aoc::input_raw(YEAR, DAY)
-        //.lines()
-        //.map(|ls| ls.parse::<_>().unwrap())
-        //.collect()
+    //.lines()
+    //.map(|ls| ls.parse::<_>().unwrap())
+    //.collect()
 }
 
-fn part1(_inputs: &str) -> u32 {
-    todo!();
+type Vec2 = nalgebra_glm::TVec2<isize>;
+
+/// Collect positions of all antennas
+fn parse_antennas(smap: &str) -> HashMap<char, Vec<Vec2>> {
+    let mut map = HashMap::<char, Vec<Vec2>>::new();
+    smap.trim().lines().enumerate().for_each(|(y, line)| {
+        line.trim().chars().enumerate().for_each(|(x, c)| {
+            if c.is_alphanumeric() {
+                map.entry(c)
+                    .and_modify(|e| {
+                        (*e).push(Vec2::new(x as isize, y as isize));
+                    })
+                    .or_insert(vec![Vec2::new(x as isize, y as isize)]);
+            }
+        })
+    });
+    map
 }
 
-fn part2(_inputs: &str) -> u32 {
-    todo!();
+/// (Solved, >1h) Given a map of antennas, represented by alphanumeric
+/// characters, count the number of unique positions for "antinodes":
+/// points on the line of two equal chars, at the same distance to the
+/// nearest one as the distance between the two points.
+fn part1(inputs: &str) -> usize {
+    let height = inputs.lines().count() as isize;
+    let width = inputs.lines().next().unwrap().len() as isize;
+    let map = parse_antennas(inputs);
+    let mut antinodes = HashSet::<Vec2>::new();
+    for (_freq, antennas) in map.iter() {
+        for a in antennas.iter() {
+            for b in antennas.iter() {
+                let d = a - b;
+                for an in [a - d, a + d].iter() {
+                    if an != b && an.x >= 0 && an.y >= 0 && an.x < width && an.y < height {
+                        antinodes.insert(*an);
+                    }
+                }
+            }
+        }
+    }
+    antinodes.len()
 }
+
+/// (Solved, >1h)
+fn part2(inputs: &str) -> usize {
+    println!("part 2");
+    let height = inputs.lines().count() as isize;
+    let width = inputs.lines().next().unwrap().len() as isize;
+    println!("H/W: {}/{}", height, width);
+    let map = parse_antennas(inputs);
+    let mut antinodes = HashSet::<Vec2>::new();
+    for (_freq, antennas) in map.iter() {
+        println!("Frequency {}", _freq);
+        for (i, a) in antennas.iter().enumerate() {
+            for b in antennas.iter().skip(i + 1) {
+                let d = a - b;
+                let mut an = *a;
+                while an.x >= 0 && an.y >= 0 && an.x < width && an.y < height {
+                    antinodes.insert(an);
+                    an += d;
+                }
+                let mut an = *a;
+                while an.x >= 0 && an.y >= 0 && an.x < width && an.y < height {
+                    antinodes.insert(an);
+                    an -= d;
+                }
+            }
+        }
+    }
+    let mut map = inputs
+        .lines()
+        .map(|line| line.chars().collect_vec())
+        .collect_vec();
+    antinodes
+        .iter()
+        .for_each(|n| map[n.y as usize][n.x as usize] = '#');
+    print_img(&map);
+    antinodes.len()
+}
+
+#[allow(unused)]
+static TEST_INPUTS: &str = "............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............";
 
 #[test]
 fn test_2024_day8_part1() {
-    // TODO
+    assert_eq!(part1(TEST_INPUTS), 14);
 }
 
 #[test]
 fn test_2024_day8_part2() {
-    // TODO
+    println!("Test part 2");
+    assert_eq!(part2(TEST_INPUTS), 34);
 }
 
 #[allow(unused)]
@@ -45,5 +138,3 @@ pub fn run() {
     println!("Took {:?}", pt_start.elapsed().unwrap());
     println!("Total time: {:?}", start.elapsed().unwrap());
 }
-
-
