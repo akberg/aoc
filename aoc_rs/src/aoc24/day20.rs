d7249dd350fbecd811546d57c1395ba5e87e4363
Andreas K. Berg <andreaskb98@gmail.com>
2024-12-20 21:52:48 +0100

    2024 day 20 solved

--- aoc_rs/src/aoc24/day20.rs
+++ aoc_rs/src/aoc24/day20.rs
@@ -1,11 +1,13 @@
 use std::collections::{BinaryHeap, HashMap, HashSet};
 
+use itertools::Itertools;
+
 use super::YEAR;
 static DAY: usize = 20;
 
 // Cloned from day16, remove direction from state.
 
-type Vec2 = nalgebra_glm::TVec2<i64>;
+type Vec2 = nalgebra_glm::TVec2<usize>;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum Direction {
@@ -18,10 +20,10 @@ impl Direction {
     pub fn delta(&self, v: Vec2, scale: usize) -> Vec2 {
         let mut v = v.clone();
         match self {
-            Direction::Up => v.y -= scale as i64,
-            Direction::Right => v.x += scale as i64,
-            Direction::Down => v.y += scale as i64,
-            Direction::Left => v.x -= scale as i64,
+            Direction::Up => v.y -= scale as usize,
+            Direction::Right => v.x += scale as usize,
+            Direction::Down => v.y += scale as usize,
+            Direction::Left => v.x -= scale as usize,
         };
         v
     }
@@ -53,8 +55,6 @@ struct QueueElement {
 impl Ord for QueueElement {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         other.cost.cmp(&self.cost)
-        // .then_with(|| self.position.cmp(&other.position))
-        // .then_with(|| self.rotation.cmp(&other.rotation))
     }
 }
 impl PartialOrd for QueueElement {
@@ -76,14 +76,14 @@ fn parse_map(inputs: &str) -> (HashSet<Vec2>, Vec2, Vec2, usize, usize) {
             match c {
                 'S' | 'E' | '.' => {
                     if c == 'S' {
-                        start.x = x as i64;
-                        start.y = y as i64;
+                        start.x = x as usize;
+                        start.y = y as usize;
                     } else if c == 'E' {
-                        end.x = x as i64;
-                        end.y = y as i64;
+                        end.x = x as usize;
+                        end.y = y as usize;
                     }
                     // Add open space
-                    map.insert(Vec2::new(x as i64, y as i64));
+                    map.insert(Vec2::new(x as usize, y as usize));
                 }
                 '#' => {
                     // Closes space
@@ -96,31 +96,12 @@ fn parse_map(inputs: &str) -> (HashSet<Vec2>, Vec2, Vec2, usize, usize) {
     (map, start, end, height, width)
 }
 
-/// Backtracking path, modified from day 16.
-fn backtrack_path(links: &HashMap<Vec2, Vec2>, start: Vec2, mut end: Vec2, path: &mut Vec<Vec2>) {
-    while end != start {
-        let prev = &links[&end];
-        path.insert(0, *prev);
-        end = *prev;
-        //backtrack_path(links, start, *prev, path);
-    }
-}
-
-/// Dijkstra shortest path, modified from day 16. There is now one single path.
-fn shortest_path(
-    map: HashSet<Vec2>,
-    start_position: Vec2,
-    end_position: Vec2,
-    known_path: &Vec<&Vec2>,
-) -> Option<(usize, Vec<Vec2>)> {
-    // Track shortest path to any point.
-    let mut links: HashMap<Vec2, Vec2> = HashMap::new();
-
-    // let mut best_length = None;
-    //let mut paths = Vec::new();
+/// Dijkstra shortest path, modified from day 16. Now find shortest path to
+/// all.
+fn shortest_path_all(map: &HashSet<Vec2>, start_position: Vec2) -> HashMap<Vec2, usize> {
     // Dist map holds a position/rotation state and its cost. Non-existing
     // entry means MAX cost.
-    let mut dist = HashMap::new();
+    let mut dist: HashMap<Vec2, usize> = HashMap::new();
     dist.insert(start_position, 0);
 
     // Queue holds states to be visited, using cost for priority.
@@ -131,34 +112,16 @@ fn shortest_path(
     });
 
     while let Some(QueueElement { cost, position }) = queue.pop() {
-        // println!("p={:?} c={}", position, cost);
-        if let Some(j) = known_path.iter().position(|&&r| r == position) {
-            // Back on known shortest path, avoid computing more.
-            // (For this puzzle we don't really care about the specific path
-            // anymore).
-            return Some((cost + (known_path.len() - j), Vec::new()));
-        }
-
-        if position == end_position {
-            // Found shortest path. Store the length
-            let mut path = Vec::with_capacity(cost);
-            path.push(position);
-            backtrack_path(&links, start_position, position, &mut path);
-            // println!("Initial shortest path ({})", path.len());
-            return Some((cost, path));
-        }
-
         for d in Direction::iterator() {
             // Check all directions
             let next_position = d.delta(position, 1);
             let next_cost = cost + 1;
 
             if map.contains(&next_position) {
+                // Open path this direction.
                 let prev_cost = dist.entry(next_position).or_insert(usize::MAX);
                 if next_cost < *prev_cost {
-                    // Path through current state is shorter than other paths to
-                    // next state.
-                    links.insert(next_position, position);
+                    // This path is shorter, update cost.
                     *prev_cost = next_cost;
                     queue.push(QueueElement {
                         cost: next_cost,
@@ -168,73 +131,69 @@ fn shortest_path(
             }
         }
     }
-    return None;
+    return dist;
 }
 
 fn input() -> String {
     crate::aoc::input_raw(YEAR, DAY)
 }
 
-fn available_shortcuts(input: &str, impact: usize) -> usize {
-    let (map, start, end, _height, _width) = parse_map(input);
-    let (cost, path) = shortest_path(map.clone(), start, end, &Vec::new()).unwrap();
-    let mut count = 0;
-    println!("Initial shortest path ({})", path.len());
+/// 1. Find shortest path from S to all, and shortes distance E to all.
+/// 2. For each position N, and for each position M with manhattan
+/// distance <= radius from M, the cost will be dist(S:N) + dist(M:E) + man(N:M)
+/// Count the number of occurences which give a cost <= dist(S:E) - 100.
+fn available_shortcuts(input: &str, short: usize, impact: usize) -> usize {
+    let (map, start, end, height, width) = parse_map(input);
 
-    for (i, &position) in path.iter().enumerate() {
-        println!("Shortcuts from step {}", i);
-        for d in Direction::iterator() {
-            // For any available shortcut, run shortest_path again
-            // - One step is a wall
-            // - Two steps is not a wall
-            if !map.contains(&d.delta(position, 1)) && map.contains(&d.delta(position, 2)) {
-                let scost = if let Some(j) = path[(i + 1)..]
-                    .iter()
-                    .position(|e| e == &d.delta(position, 2))
-                {
-                    // Shortcut in known path
-                    let scost = cost - j + 1;
-                    print!(
-                        " Cuts to later (+{:>5}) in original path. exp.cost={:>5}",
-                        j, scost
-                    );
-                    scost
-                } else {
-                    // Possible shortcut through unchecked paths, recompute from current
-                    // position.
-                    print!(" New path.                                              ");
-                    let mut smap = map.clone();
-                    smap.insert(d.delta(position, 1));
-                    let (scost, _spath) = shortest_path(
-                        smap,
-                        position,
-                        end,
-                        &path.iter().skip(i + 1).collect::<Vec<_>>(),
-                    )
-                    .unwrap();
-                    scost + i
-                };
-                print!(" cost={:<5}", scost);
-                if scost < cost {
-                    println!(" Saving {}", cost - scost);
-                    if cost - scost >= impact {
-                        count += 1;
+    let from_s = shortest_path_all(&map, start);
+    let from_e = shortest_path_all(&map, end);
+    let s_to_e = from_s[&end];
+
+    let mut jumps_set = HashSet::new();
+
+    (0..height).cartesian_product(0..width).for_each(|(y, x)| {
+        // From each available tile to jump from
+        if let Some(s) = map.get(&Vec2::new(x, y)) {
+            for j in 0..=short {
+                for i in 0..=(short - j) {
+                    let mut jumps = Vec::from([Vec2::new(x + i, y + j)]);
+                    let dx = x >= i;
+                    let dy = y >= j;
+                    if dx {
+                        jumps.push(Vec2::new(x - i, y + j));
                     }
-                } else {
-                    println!("");
+                    if dy {
+                        jumps.push(Vec2::new(x + i, y - j));
+                    }
+                    if dx && dy {
+                        jumps.push(Vec2::new(x - i, y - j));
+                    }
+                    for pos_j in jumps {
+                        // For each available landing tile within
+                        if let Some(e) = map.get(&pos_j) {
+                            let cost = from_s[s] + from_e[e] + i + j;
+                            if cost <= s_to_e - impact {
+                                jumps_set.insert((s, e));
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-    count
+    });
+    jumps_set.len()
 }
 
+/// (Solved, >1h) Replay of shortest path, adding a post-processing to find
+/// walls to go through.
 fn part1(inputs: &str) -> usize {
-    available_shortcuts(inputs, 100)
+    available_shortcuts(inputs, 2, 100)
 }
 
-fn part2(_inputs: &str) -> u32 {
-    todo!();
+/// (Solved, >2h) Modified constraints required complete rewrite to final
+/// solution.
+fn part2(inputs: &str) -> usize {
+    available_shortcuts(inputs, 20, 100)
 }
 
 #[allow(unused)]
@@ -257,9 +216,9 @@ static TEST_INPUT: &str = "###############
 #[test]
 fn test_2024_day20_part1() {
     println!("Too cool for testing");
-    // assert_eq!(available_shortcuts(TES_INPUTS, 2), 14);
-    // assert_eq!(available_shortcuts(TES_INPUTS, 4), 28);
-    // assert_eq!(available_shortcuts(TES_INPUTS, 4), 28);
+    assert_eq!(available_shortcuts(TEST_INPUT, 2, 64), 1);
+    // assert_eq!(available_shortcuts(TEST_INPUT, 4), 28);
+    // assert_eq!(available_shortcuts(TEST_INPUT, 4), 28);
 }
 
 #[test]
