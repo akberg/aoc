4948cd823599397463249834359be7b27089d803
Andreas K. Berg <andreaskb98@gmail.com>
2025-12-18 10:01:55 +0100

    Day3 3 complete, day2 p1 complete

--- aoc_rs/src/aoc25/day02.rs
+++ aoc_rs/src/aoc25/day02.rs
@@ -1,24 +1,75 @@
+use std::ops::Range;
+
 use super::YEAR;
 static DAY: usize = 02;
 
-fn input() -> String {
-    crate::aoc::input_raw(YEAR, DAY)
-        //.lines()
-        //.map(|ls| ls.parse::<_>().unwrap())
-        //.collect()
+fn parse_input(line: String) -> Vec<Range<i64>> {
+    line.split(",").map(|rng| {
+        let (s, e) = rng.split_once("-").unwrap();
+        s.parse::<i64>().unwrap()..(e.parse::<i64>().unwrap() + 1)
+    }).collect()
 }
 
-fn part1(_inputs: &str) -> u32 {
-    todo!();
+fn input() -> Vec<Range<i64>> {
+    parse_input(crate::aoc::input_raw(YEAR, DAY))
 }
 
-fn part2(_inputs: &str) -> u32 {
-    todo!();
+fn magnitude(n: i64) -> u32 {
+    let mut m = 0;
+    while 10i64.pow(m + 1) < n {
+        m += 1;
+    }
+    m
+}
+
+// Find numbers containing a digit sequence repeated twice.
+fn part1(inputs: &Vec<Range<i64>>) -> i64 {
+    let mut count = 0;
+    for rng in inputs.clone() {
+        for i in rng {
+            for j in 1..(i / 2) {
+                let m = magnitude(j);
+                for n in 2.. {
+                    // j + j * 10.pow(m) + j * 10.pow(2 * m) ...;
+                }
+            }
+
+            let m = magnitude(i);
+            if m % 2 == 0 { continue; }
+            let half = (m+1) / 2;
+            let lower = i / 10i64.pow(half);
+            if i == lower + (lower * 10i64.pow(half)) {
+                count += i;
+            }
+        }
+    }
+    return count
+}
+
+// Find numbers containing a digit sequence repeated twice.
+fn part2(inputs: &Vec<Range<i64>>) -> i64 {
+    let mut count = 0;
+    for rng in inputs.clone() {
+        for i in rng {
+            let m = magnitude(i);
+            let half = (m+1) / 2;
+            let lower = i / 10i64.pow(half);
+            if i == lower + (lower * 10i64.pow(half)) {
+                count += i;
+            }
+        }
+    }
+    return count
 }
 
 #[test]
 fn test_2025_day2_part1() {
-    // TODO
+    assert_eq!(magnitude(1), 0);
+    assert_eq!(magnitude(15), 1);
+    assert_eq!(magnitude(88), 1);
+    assert_eq!(magnitude(359), 2);
+    let inputs = parse_input(String::from("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"));
+    assert_eq!(part1(&inputs), 1227775554);
 }
 
 #[test]
--- aoc_rs/src/aoc25/day03.rs
+++ aoc_rs/src/aoc25/day03.rs
@@ -1,29 +1,53 @@
+use std::ops::Index;
+
 use super::YEAR;
 static DAY: usize = 03;
 
 fn input() -> String {
     crate::aoc::input_raw(YEAR, DAY)
-        //.lines()
-        //.map(|ls| ls.parse::<_>().unwrap())
-        //.collect()
 }
 
-fn part1(_inputs: &str) -> u32 {
-    todo!();
+fn part1(inputs: &str) -> u32 {
+    inputs.lines().map(|line| {
+        let line = line.trim().chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<_>>();
+        let a = line[0..line.len()-1].iter().max().unwrap();
+        let idx: usize = line.iter().position(|c| c==a).unwrap();
+        let b = line[idx+1..].iter().max().unwrap();
+        println!("{} {}", a, b);
+        a * 10 + b
+    }).sum()
 }
 
-fn part2(_inputs: &str) -> u32 {
-    todo!();
+fn part2(inputs: &str) -> u64 {
+    inputs.lines().map(|line| {
+        let line = line.trim().chars().map(|c| c.to_digit(10).unwrap() as u64).collect::<Vec<_>>();
+        let mut res = 0;
+        let mut prev = 0;
+        for i in line.len()-12 .. line.len() {
+            let a = line[prev..=i].iter().max().unwrap();
+            prev = line.iter().skip(prev).position(|c| c==a).unwrap() + 1 + prev;
+            res = res * 10 + a;
+        }
+        res
+    }).sum()
 }
 
 #[test]
 fn test_2025_day3_part1() {
-    // TODO
+    let inputs = "987654321111111
+                811111111111119
+                234234234234278
+                818181911112111";
+    assert_eq!(part1(inputs), 357);
 }
 
 #[test]
 fn test_2025_day3_part2() {
-    // TODO
+    let inputs = "987654321111111
+                811111111111119
+                234234234234278
+                818181911112111";
+    assert_eq!(part2(inputs), 3121910778619);
 }
 
 #[allow(unused)]
